a49ab4fca25df82b24d26222d3d7697776948535
Vinicius Torres <vinicius.torres@innovatismc.com>
2025-12-05 16:23:25 -0300

    Iniciamos o projeto

---
+++ src/main/java/br/com/gopro/api/ApiDaGoproApplication.java
@@ -0,0 +1,13 @@
+package br.com.gopro.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDaGoproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDaGoproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gopro/api/ApiDaGoproApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gopro.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDaGoproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
