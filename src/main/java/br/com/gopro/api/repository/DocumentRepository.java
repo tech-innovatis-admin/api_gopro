599138c063cc2f397af26473083fb8cc8ad471ba
Gabriel Vitor <gabrielvitormchaves@gmail.com>
2025-12-17 14:51:43 -0300

    feat(repository): criação do repository de Document

---
+++ src/main/java/br/com/gopro/api/repository/DocumentRepository.java
@@ -0,0 +1,9 @@
+package br.com.gopro.api.repository;
+
+import br.com.gopro.api.model.Document;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DocumentRepository extends JpaRepository<Long, Document> {
+}
