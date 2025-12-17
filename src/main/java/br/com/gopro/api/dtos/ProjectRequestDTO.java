4fc0a34234da1b473b2d62641dd94d35156f6851
Gabriel Vitor <gabrielvitormchaves@gmail.com>
2025-12-17 16:07:33 -0300

    feat(dtos): implementação do DTO ProjectRequest

--- src/main/java/br/com/gopro/api/dtos/ProjectRequestDTO.java
+++ src/main/java/br/com/gopro/api/dtos/ProjectRequestDTO.java
@@ -1,6 +1,46 @@
 package br.com.gopro.api.dtos;
 
-public record ProjectRequestDTO(
+import br.com.gopro.api.enums.StatusProjectsEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record ProjectRequestDTO(
+        @NotBlank(message = "Nome é obrigatório!")
+        String name,
+
+        String code,
+
+        @NotBlank(message = "Status do Projeto é obrigatório")
+        StatusProjectsEnum statusProjects,
+
+        String areaSegmento,
+
+        @NotNull(message = "Orgão financiador é obrigatório")
+        Long orgaoFinanciador,
+
+        @NotNull(message = "Orgão executor é obrigatório")
+        Long executionOrg,
+
+        String cordinator,
+
+        String scope,
+
+        BigDecimal contractValue,
+
+        LocalDate startDate,
+
+        LocalDate endDate,
+
+        LocalDate openingDate,
+
+        String executionLocation,
+
+        @NotNull(message = "Usuário que criou o projeto é obrigatório")
+        Long createdBy,
+
+        Long updatedBy
 ) {
 }
