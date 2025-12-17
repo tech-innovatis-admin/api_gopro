67faeb16a37ab1d273a722b816ae19c4b68078db
Gabriel Vitor <gabrielvitormchaves@gmail.com>
2025-12-17 16:09:24 -0300

    feat(dtos): implementação do DTO ProjectResponse

--- src/main/java/br/com/gopro/api/dtos/ProjectResponseDTO.java
+++ src/main/java/br/com/gopro/api/dtos/ProjectResponseDTO.java
@@ -1,4 +1,28 @@
 package br.com.gopro.api.dtos;
 
-public record ProjectResponseDTO() {
+import br.com.gopro.api.enums.StatusProjectsEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record ProjectResponseDTO(
+        Long id,
+        String name,
+        String code,
+        StatusProjectsEnum statusProjects,
+        String areaSegmento,
+        Long orgaoFinanciador,
+        Long executionOrg,
+        String cordinator,
+        String scope,
+        BigDecimal contractValue,
+        LocalDate startDate,
+        LocalDate endDate,
+        LocalDate openingDate,
+        String executionLocation,
+        Long createdBy,
+        Long updatedBy
+) {
 }
