1e4b50515acb2c593d2b0d059ec6cb816995de7b
Gabriel Vitor <gabrielvitormchaves@gmail.com>
2025-12-18 10:39:32 -0300

    feat(dto's): criação do response dto de People

---
+++ src/main/java/br/com/gopro/api/dtos/PeopleResponseDTO.java
@@ -0,0 +1,20 @@
+package br.com.gopro.api.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDate;
+
+public record PeopleResponseDTO(
+        String fullName,
+        String cpf,
+        String email,
+        String phone,
+        LocalDate birthDate,
+        String address,
+        String zipCode,
+        String city,
+        String state,
+        String notes
+) {
+}
