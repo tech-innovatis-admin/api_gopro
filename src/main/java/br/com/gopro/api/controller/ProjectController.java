8cf63273ac54ba8da243aff7cfafb3211bcbcb18
Gabriel Vitor <gabrielvitormchaves@gmail.com>
2025-12-18 09:11:34 -0300

    feat(controller): Criação dos Endpoints POST, GET, GET BY ID, PUT e DELETE de ProjectController

---
+++ src/main/java/br/com/gopro/api/controller/ProjectController.java
@@ -0,0 +1,50 @@
+package br.com.gopro.api.controller;
+
+import br.com.gopro.api.dtos.ProjectRequestDTO;
+import br.com.gopro.api.dtos.ProjectResponseDTO;
+import br.com.gopro.api.service.ProjectService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/projects")
+@RequiredArgsConstructor
+public class ProjectController {
+
+    private final ProjectService projectService;
+
+    @PostMapping
+    public ResponseEntity<ProjectResponseDTO> createProject(@Valid @RequestBody ProjectRequestDTO dto) {
+        ProjectResponseDTO projectCreated =projectService.createProject(dto);
+        return ResponseEntity.status(201).body(projectCreated);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProjectResponseDTO>> listAllProjects() {
+        return ResponseEntity.ok(projectService.listAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProjectResponseDTO> listProjectById(@PathVariable Long id) {
+        return ResponseEntity.ok(projectService.listProjectById(id));
+    }
+
+    @PutMapping("/id")
+    public ResponseEntity<ProjectResponseDTO> updateProjectById(
+            @PathVariable Long id,
+            @Valid @RequestBody ProjectRequestDTO dto
+    ) {
+        ProjectResponseDTO projectUpdated = projectService.updateProject(id, dto);
+        return ResponseEntity.ok(projectUpdated);
+    }
+
+    @DeleteMapping("/id")
+    public ResponseEntity<Void> deleteProject(@PathVariable Long id) {
+        projectService.deleteProject(id);
+        return ResponseEntity.noContent().build();
+    }
+}
