52682e5bfa0cfb8224953deda2b8afa9bff8273a
Gabriel Vitor Moreno Chaves <gabrielvitormchaves@gmail.com>
2026-01-06 15:19:10 -0300

    feat(repository): criação do repository de ProjectPeople

---
+++ src/main/java/br/com/gopro/api/repository/ProjectPeopleRepository.java
@@ -0,0 +1,9 @@
+package br.com.gopro.api.repository;
+
+import br.com.gopro.api.model.ProjectPeople;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectPeopleRepository extends JpaRepository<ProjectPeople, Long> {
+}
