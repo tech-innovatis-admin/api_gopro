db6a81607dee051effe2425501aeb0646d219eb9
Gabriel Vitor Moreno Chaves <gabrielvitormchaves@gmail.com>
2026-01-07 16:09:09 -0300

    config: implementação do Swagger para documentar o controller

---
+++ src/main/java/br/com/gopro/api/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package br.com.gopro.api.config;
+
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("GoPro 2 - API")
+                        .description("API Backend do Sistema GoPro 2")
+                        .version("v1")
+                        .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0")));
+//                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+//                .components(new Components().addSecuritySchemes("bearerAuth",
+//                        new SecurityScheme()
+//                                .name("bearerAuth")
+//                                .type(SecurityScheme.Type.HTTP)
+//                                .scheme("bearer")
+//                                .bearerFormat("JWT")));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
