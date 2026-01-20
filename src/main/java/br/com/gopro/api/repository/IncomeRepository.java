5d73742b9b2b0152f10f5f3d9e01a7a41c34dbf7
Gabriel Vitor <gabrielvitormchaves@gmail.com>
2026-01-20 16:59:46 -0300

    feat(repository): criação do repository de Income

---
+++ src/main/java/br/com/gopro/api/repository/IncomeRepository.java
@@ -0,0 +1,9 @@
+package br.com.gopro.api.repository;
+
+import br.com.gopro.api.model.Income;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IncomeRepository extends JpaRepository<Income, Long> {
+}
