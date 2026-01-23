44de576fe24715e76fad1c79a892e38c6fe27c57
Gabriel Vitor <gabrielvitormchaves@gmail.com>
2026-01-23 12:29:41 -0300

    feat(repository): criação do repository de Espense

---
+++ src/main/java/br/com/gopro/api/repository/ExpenseRepository.java
@@ -0,0 +1,9 @@
+package br.com.gopro.api.repository;
+
+import br.com.gopro.api.model.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+}
